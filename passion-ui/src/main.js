83465e87851a7a16bceda78f5424e0f4830bb260
chiyu999 <904471750@qq.com>
2023-05-05 14:57:48 +0800

    第三方图标库/多语言国际化测试-测试完成

--- passion-ui/src/main.js
+++ passion-ui/src/main.js
@@ -4,13 +4,15 @@ import Vue, { h } from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import i18n from './i18n'
 import api from './http' //导入api模块
 import global from '@/utils/global'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 Vue.use(ElementUI) //注册使用element
 Vue.use(api) //注册使用api,可以通过this.$api.子模块.方法 的方式调用后台接口
 
@@ -19,6 +21,7 @@ Vue.prototype.global = global //挂载全局配置模块
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   render: h => h(App)
   // components: { App },
