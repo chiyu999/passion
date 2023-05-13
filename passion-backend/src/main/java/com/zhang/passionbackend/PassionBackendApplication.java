a73d1a8554bb3db505bacac64007028828cda2d1
chiyu999 <904471750@qq.com>
2023-05-13 23:21:29 +0800

    集成Swagger文档

--- passion-backend/src/main/java/com/zhang/passionbackend/PassionBackendApplication.java
+++ passion-backend/src/main/java/com/zhang/passionbackend/PassionBackendApplication.java
@@ -3,6 +3,12 @@ package com.zhang.passionbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Description Swagger配置
+ * @author chixiaoyu
+ * @date 2023/05/13
+ */
+
 @SpringBootApplication
 public class PassionBackendApplication {
 
